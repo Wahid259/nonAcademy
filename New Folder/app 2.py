d556c46fd984453acf77f7766760074d7dd5d095
Wahid259 <wahid259raj@gmail.com>
2026-01-16 21:44:13 +0600

    git clone & update files & folder

--- New Folder/app 2.py
+++ New Folder/app 2.py
@@ -1,1 +1,2 @@
 print("Hello World from New Folder")
+print("Hello World from New Folder & Python World")
--- app.py
+++ app.py
@@ -1,5 +1,3 @@
 print ("Hello World")
-
 print ("Hello World 2, Welcome to Python")
-
-
+print ("Hello World 3, Welcome to Python world")
